92d9f5bf350bf3cd447626547c0670d94b36d764
Darren Mulholland <dmulholl@tcd.ie>
2021-03-21 15:22:29 +0000

    Snapshot

--- clox/src/memory.c
+++ clox/src/memory.c
@@ -76,8 +76,16 @@ static void blackenObject(Obj* object) {
 
     switch (object->type) {
         case OBJ_CLASS: {
+            case OBJ_BOUND_METHOD: {
+                ObjBoundMethod* bound = (ObjBoundMethod*)object;
+                markValue(bound->receiver);
+                markObject((Obj*)bound->method);
+                break;
+            }
+
             ObjClass* klass = (ObjClass*)object;
             markObject((Obj*)klass->name);
+            markTable(&klass->methods);
             break;
         }
 
@@ -120,7 +128,13 @@ static void freeObject(Obj* object) {
 #endif
 
     switch (object->type) {
+        case OBJ_BOUND_METHOD:
+            FREE(ObjBoundMethod, object);
+            break;
+
         case OBJ_CLASS: {
+            ObjClass* klass = (ObjClass*)object;
+            freeTable(&klass->methods);
             FREE(ObjClass, object);
             break;
         }
